85c1a2fb56f352b17f28e02350fd1fd889c0cd7b
Pete Stewart <petestew@gmail.com>
2020-12-14 15:51:20 -0600

    added profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -13,11 +13,23 @@ export const Profile = props => {
 
     return (
         <main className="profile-container px-3">
-        <h3 className="mt-3 text-center">Your Profile</h3>
-            <section>
-            (PROFILE HERE)
-            
-            </section>
+        <h2 className="mt-3 text-center">Your Profile</h2>
+            <div className="container mt-3">
+                <div className="profile-header row">
+                    <div className="col-4">
+                        <img className="profile-page-pic w-100" src={profile.profile_pic !== '' ? profile.profile_pic : 'https://cdn3.iconfinder.com/data/icons/web-ui-3/128/Account-2-256.png'} alt="profile pic"/>
+                    </div>
+                    <div className="col-8">
+                        <h3 className="my-0">{profile.full_name}</h3>
+                        {profile.location}
+                    </div>
+                </div>
+            <div className="profile-bio row mt-2">
+                <div className="col-12">
+                    {profile.bio}
+                </div>
+            </div>
+            </div>
     </main>
     )
 }
