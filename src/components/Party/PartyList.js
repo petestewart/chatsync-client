17cb76ae0eb3a809498e21e6f95085bafba25f21
Pete Stewart <petestew@gmail.com>
2020-12-16 11:22:40 -0600

    party list populates with a users parties

--- src/components/Party/PartyList.js
+++ src/components/Party/PartyList.js
@@ -8,12 +8,20 @@ export const PartyList = props => {
 
     useEffect(getUpcomingParties, [])
 
+    if (upcomingParties.length > 0) {
+        const partyList = upcomingParties.map((party, index) => <PartyCard party={party} key={index} />);
+      }
+
 
     return (
         <main className="profile-container px-3">
             <h2 className="mt-3 text-center">Upcoming Events</h2>
             <section className="mt-5">
-                <PartyCard party={upcomingParties[0]} />
+                { upcomingParties.length > 0
+                    ? upcomingParties.map((party, index) => <PartyCard party={party} key={index} />)
+                    
+                    : ''
+                }
             </section>
         </main>
             
