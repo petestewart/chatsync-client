6455090306b0ae0ca2f545bf373b9686e003fec3
Pete Stewart <petestew@gmail.com>
2021-01-03 19:40:56 -0600

    party end times affect partys live status

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -67,7 +67,7 @@ export const Home = props => {
             <section className="mt-5 text-center">
                 {userPartiesMessage()}
                 <button className="btn btn-primary w-100 mt-3" onClick={() => {props.history.push('/parties/create')}}>Schedule WatchParty</button>
-                <button className="btn btn-primary w-100 mt-3" onClick={() => {props.history.push('/parties/create')}}>Launch Instant WatchParty</button>
+                <button className="btn btn-primary w-100 mt-3 mb-3" onClick={() => {props.history.push('/parties/create')}}>Launch Instant WatchParty</button>
                 
             </section>
         </main>
